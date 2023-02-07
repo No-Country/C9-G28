3551e068176320e14fd295e264f57a2de9bb1bc5
Josue O. Monge <razuordonez@gmail.com>
2023-02-07 10:39:08 -0500

    reducers

---
+++ client/src/data/dummy.js
@@ -0,0 +1,26 @@
+const dummy = [
+  {
+    name: 'Juan Sanchez',
+    email: 'juanSanchez@email.com',
+    password: '12345678',
+    userRol: 'Admin',
+  },
+  {
+    name: 'Martha Rodriguez',
+    email: 'martRodriguez@email.com',
+    password: '12345678',
+    userRol: 'User',
+  },
+  {
+    name: 'Carlos Cueva',
+    email: 'carlosCueva@email.com',
+    password: '12345678',
+    userRol: 'Staff',
+  },
+  {
+    name: 'Maria Castro',
+    email: 'mariaCastro@email.com',
+    password: '12345678',
+    userRol: 'Staff',
+  },
+];
